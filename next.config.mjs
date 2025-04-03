abd54da16c7fbde457b510acbb3d4c068b6e6e2c
shun <shun.soccer.shun@gmail.com>
2025-04-03 22:15:29 +0900

    blog: fix-content

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,21 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gyazo.com",
+      },
+      {
+        protocol: "https",
+        hostname: "t1.gstatic.com",
+      },
+    ],
+  },
   reactStrictMode: true,
 };
 
